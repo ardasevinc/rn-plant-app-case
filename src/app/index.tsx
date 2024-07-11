1495282c94082ef8551d5989a548d3dd74538a7b
Arda Sevinç <arda@ardasevinc.com>
2024-07-11 19:28:49 +0300

    chore: move all source code under src/

---
+++ src/app/index.tsx
@@ -0,0 +1,6 @@
+import { Text, View } from 'react-native';
+import tw from 'twrnc';
+
+export default function Index() {
+  return <View style={tw`bg-black flex-1`}></View>;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./app/**/*.tsx'],
+  content: ['src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      black: 'hsla(150, 29%, 11%, 1)',
+      primary: {
+        DEFAULT: 'hsla(151, 63%, 42%, 1)',
+        foreground: 'hsla(0, 0%, 100%, 1)',
+      },
+      muted: {
+        DEFAULT: 'hsla(150, 12%, 40%, 0.7)',
+        alt: 'hsla(150, 29%, 11%, 0.7)',
+        home: 'hsla(240, 0%, 59%, 1)',
+      },
+    },
   },
   plugins: [],
 };
