daf39c1341e10863bb7cd35df39b1d1f95fb6196
Arda Sevinç <arda@ardasevinc.com>
2024-07-13 18:05:16 +0300

    feat: provide types to rtk api query builder

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,5 @@
+import { CategoriesApiResponse } from '@/types/category.type';
+import { QuestionsApiResponse } from '@/types/question.type';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
@@ -6,10 +8,10 @@ export const apiSlice = createApi({
     baseUrl: 'https://dummy-api-jtg6bessta-ey.a.run.app',
   }),
   endpoints: (builder) => ({
-    getQuestions: builder.query({
+    getQuestions: builder.query<QuestionsApiResponse, undefined>({
       query: () => '/getQuestions',
     }),
-    getCategories: builder.query({
+    getCategories: builder.query<CategoriesApiResponse, undefined>({
       query: () => '/getCategories',
     }),
   }),
