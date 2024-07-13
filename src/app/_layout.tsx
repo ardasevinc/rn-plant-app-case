1745fe11612666ed83a5ca52fb2c563a88498246
Arda Sevinç <arda@ardasevinc.com>
2024-07-13 11:08:11 +0300

    feat: set splash screen until root view is rendered
    extra: by using expo-splash-screen we make sure that
    splash screen is not hidden until bootApp function has executed
    AND root view is rendered (through onLayout event). Then, we mock
    the skipOnboarding flag to configure the router to skip the onboarding
    if it has been completed.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,44 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { View } from 'react-native';
-import { Slot } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { router, Slot } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import tw, { useDeviceContext } from 'twrnc';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   useDeviceContext(tw);
+  const [appReady, setAppReady] = useState(true);
+  const skipOnboarding = useRef(true);
+
+  useEffect(() => {
+    async function bootApp() {
+      try {
+        if (skipOnboarding.current === false) {
+          router.replace('/onboarding');
+        }
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setAppReady(true);
+      }
+    }
+    bootApp();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appReady]);
+
+  if (!appReady) return null;
 
   return (
     <SafeAreaProvider>
-      <View style={tw`flex-1`}>
+      <View style={tw`flex-1`} onLayout={onLayoutRootView}>
         <Slot />
       </View>
     </SafeAreaProvider>
