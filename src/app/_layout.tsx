0c4e04073bb14429e1ab58b74dc23c34e7cb3527
Arda Sevinç <arda@ardasevinc.com>
2024-07-14 14:41:09 +0300

    feat: add expo-image & load fonts for expo go

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,6 +6,11 @@ import * as SplashScreen from 'expo-splash-screen';
 import tw, { useDeviceContext } from 'twrnc';
 import { getOnboardingCompleted } from '@/utils/storage';
 
+import { loadAsync } from 'expo-font';
+
+// Enable when using the development build
+// import 'expo-dev-client';
+
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -15,8 +20,21 @@ export default function RootLayout() {
   useEffect(() => {
     async function bootApp() {
       try {
-        const onboardingCompleted = await getOnboardingCompleted();
+        // load fonts - comment when using expo dev build
+        await loadAsync({
+          'SFProText-Regular': require('src/assets/fonts/SFProText-Regular.otf'),
+          'SFProText-Semibold': require('src/assets/fonts/SFProText-Semibold.otf'),
+          'SFProText-Bold': require('src/assets/fonts/SFProText-Bold.otf'),
+          'Rubik-Light': require('src/assets/fonts/Rubik-Light.ttf'),
+          'Rubik-Regular': require('src/assets/fonts/Rubik-Regular.ttf'),
+          'Rubik-Medium': require('src/assets/fonts/Rubik-Medium.ttf'),
+          'Rubik-SemiBold': require('src/assets/fonts/Rubik-SemiBold.ttf'),
+          'Rubik-Bold': require('src/assets/fonts/Rubik-Bold.ttf'),
+          'Rubik-Black': require('src/assets/fonts/Rubik-Black.ttf'),
+          'FONTSPRINGDEMO-VisbyCFExtraBoldRegular': require('src/assets/fonts/FONTSPRINGDEMO-VisbyCFExtraBoldRegular.otf'),
+        });
 
+        const onboardingCompleted = await getOnboardingCompleted();
         if (!onboardingCompleted) {
           router.replace('/onboarding');
         }
